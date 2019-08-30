4e833e738f4cf0c13b6732ee2df6851051a0f52b
Julien PELUSO <julien.peluso@cgi.com>
2019-08-30 15:25:28 +0200

    projet ionic

---
+++ src/app/flashcard/flashcard.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flashcard',
+  templateUrl: './flashcard.component.html',
+  styleUrls: ['./flashcard.component.scss'],
+})
+export class FlashcardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
